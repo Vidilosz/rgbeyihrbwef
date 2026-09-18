338c95cc634b6cd19caa83c35d3933bfb8a8b307
Bence <en.molnar.bence@gmail.com>
2026-09-18 10:10:18 +0200

    oravege

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import fs from 'fs';
+import { Criminal } from './Criminal.js';
 
 @Controller()
 export class AppController {
@@ -12,4 +14,41 @@ export class AppController {
       title: 'My First NestJS App'
     }
   }
+
+  @Get('piros-kek')
+  @Render('red-blue')
+  getRedBlue() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let bgColor = randomNumber > 50 ? 'red' : 'blue';
+    return {
+      bgColor
+    }
+  }
+
+  @Get('wanted')
+  @Render('wanted')
+  getWanted() {
+    const criminal = JSON.parse(fs.readFileSync('wanted.json', {encoding: 'utf-8'})) as Criminal;
+    return { criminal };
+  }
+
+  @Get('search')
+  @Render('search')
+  searchCrime(@Query('keresett') keresett: string) {
+    if (!keresett){
+      return {
+        talalatok:[]
+      }
+    }
+
+    const criminal = JSON.parse(
+    fs.readFileSync('wanted.json', {encoding: 'utf-8'})
+    ) as Criminal;
+
+    return{
+      talalatok: criminal.crimes.filter
+      (c => c.toLocaleLowerCase().includes(keresett.toLocaleLowerCase()))
+    }
+  }
+
 }
